08823c2d47582c5881004a274106002bdb3763bb
sshah98 <shahsuraj261@gmail.com>
2018-06-04 14:49:01 -0400

    staging commits

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, request, redirect, url_for, g
+from pager import Pager
+# import sqlite3
+import datetime
 
+
+STATIC_FOLDER = 'static'
+APPNAME = 'AMOS East'
+
+app = Flask(__name__, static_folder=STATIC_FOLDER)
+app.config.update(APPNAME=APPNAME,)
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+def homepage():
+    
+    camera_count = 10
+    return render_template('home.html', camera_count=camera_count)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
