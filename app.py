04d863e04674a0131814d8ba79a02c20a7329948
sshah98 <shahsuraj261@gmail.com>
2018-06-04 14:58:16 -0400

    added changes to other buttons

--- app.py
+++ app.py
@@ -13,8 +13,27 @@ app.config.update(APPNAME=APPNAME,)
 @app.route('/')
 def homepage():
     
+    # TODO: change camera count to get database number
     camera_count = 10
     return render_template('home.html', camera_count=camera_count)
+    
+@app.route('/about')
+def aboutpage():
+    return render_template('about.html')
+
+@app.route('/map')
+def mappage():
+    # implement google map api functionality here
+    return render_template('map.html')
+    
+@app.route('/submitcam')
+def sumbitcam():
+    return render_template('submitcam.html')
+
+@app.route('/moreinfo')
+def moreinfo():
+    return render_template('moreinfo.html')
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
