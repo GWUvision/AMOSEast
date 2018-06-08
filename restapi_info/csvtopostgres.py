713a0c5e5a49587d8e71bc6e780f421878e4f203
krood20 <krood20@gwmail.gwu.edu>
2018-06-08 13:10:17 -0400

    debugging

--- restapi_info/csvtopostgres.py
+++ restapi_info/csvtopostgres.py
@@ -1,43 +1,21 @@
 import os
 import psycopg2
 
-<<<<<<< HEAD
 DATABASE_URL = os.environ['DATABASE_URL']
 database = psycopg2.connect(DATABASE_URL, sslmode='require')
-=======
->>>>>>> 6a744b9fcb4d3239dc104eca6b566347cf971660
 
-def copied():
+cur = database.cursor()
 
-    DATABASE_URL = os.environ['DATABASE_URL']
-    database = psycopg2.connect(DATABASE_URL, sslmode='require')
+print("Connected to database...")
 
-<<<<<<< HEAD
 try:
     cur.execute("""COPY cameras(cameraid, name, url, latitude, longitude, last_width, last_height)
             FROM '/pless_nfs/home/krood20/AMOSEast/restapi_info/data2.csv' DELIMITER ',' CSV HEADER """)
     print("Copied data from csv to postgres database...")
 
-except:
+except Exception as e:
     print ("Unable to copy data...")
+    print(e)
 
 
 database.commit()
-=======
-    cur = database.cursor()
-
-    print("Connected to database...")
-
-    try:
-        cur.execute("""COPY cameras(cameraid, name, url, latitude, longitude, last_width, last_height)
-                FROM '/home/suraj/Documents/GWU/AMOSEast/restapi_info/data2.csv' DELIMITER ',' CSV HEADER """)
-        print("Copied data from csv to postgres database...")
-
-    except:
-        print ("Unable to copy data...")
-
-    database.commit()
-
-
-copied()
->>>>>>> 6a744b9fcb4d3239dc104eca6b566347cf971660
