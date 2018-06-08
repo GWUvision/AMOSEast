6a744b9fcb4d3239dc104eca6b566347cf971660
sshah98 <shahsuraj261@gmail.com>
2018-06-08 10:53:42 -0400

    added images and fixed issues

--- restapi_info/csvtopostgres.py
+++ restapi_info/csvtopostgres.py
@@ -1,16 +1,16 @@
 import os
 import psycopg2
 
-#DATABASE_URL = os.environ['DATABASE_URL']
-#database = psycopg2.connect(DATABASE_URL, sslmode='require')
-
-cur = database.cursor()
-
-print("Connected to database...")
-
-<<<<<<< HEAD
 
 def copied():
+
+    DATABASE_URL = os.environ['DATABASE_URL']
+    database = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+    cur = database.cursor()
+
+    print("Connected to database...")
+
     try:
         cur.execute("""COPY cameras(cameraid, name, url, latitude, longitude, last_width, last_height)
                 FROM '/home/suraj/Documents/GWU/AMOSEast/restapi_info/data2.csv' DELIMITER ',' CSV HEADER """)
@@ -23,18 +23,3 @@ def copied():
 
 
 copied()
-=======
-try:
-    cur.execute("""COPY cameras(cameraid, name, url, latitude, longitude, last_width, last_height)
-            FROM '/home/suraj/Documents/GWU/AMOSEast/restapi_info/data2.csv' DELIMITER ',' CSV HEADER """)
-    print("Copied data from csv to postgres database...")
-
-except:
-    print ("Unable to copy data...")
-
-
-database.commit()
-<<<<<<< HEAD
-=======
->>>>>>> 4ef86d78142d33a79126b693da3bc17070da1d06
->>>>>>> dev
