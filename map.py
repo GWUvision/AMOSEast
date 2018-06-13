30aad4d23344ca940ea32383a4c57c941d635835
sshah98 <shahsuraj261@gmail.com>
2018-06-13 14:22:53 -0400

    small change

--- map.py
+++ map.py
@@ -1,11 +1,32 @@
-import sqlite3
+import psycopg2
+import os
+from weather import Weather, Unit
 
-DATABASE = 'allimages.db'
-db = sqlite3.connect(DATABASE)
-conn = db.cursor()
-all_cameras = "SELECT latitude, longitude FROM image_info"
-conn.execute(all_cameras)
-data = conn.fetchall()
 
-print(data[0])
+
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require').cursor()
+all_cameras_query = "SELECT cameraid, name, url, latitude, longitude FROM cameras ORDER BY cameraid"
+conn.execute(all_cameras_query)
+all_cameras = conn.fetchall()
+
+print(all_cameras[0][3])
+print(all_cameras[0][4])
+
+weather = Weather(Unit.FAHRENHEIT)
+
+for cameras in all_cameras:
+    # print(cameras[3])
+    try:
+        print(cameras)
+        lookup = weather.lookup_by_latlng(cameras[3],cameras[4])
+        condition = lookup.condition
+        print(condition.temp)
+    except AttributeError as e:
+        print("No latitude/longitude given")
+    except KeyError as e:
+        print(e)
+        
+    
+    
 
