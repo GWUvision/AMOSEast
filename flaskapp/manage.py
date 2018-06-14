d55695ce2d0450bf9518ece47334e125bb508dc1
sshah98 <shahsuraj261@gmail.com>
2018-06-14 10:56:06 -0400

    added flaskapp folder which works with migrations

---
+++ flaskapp/manage.py
@@ -0,0 +1,16 @@
+import os
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+from app import app, db
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
+
+if __name__ == '__main__':
+    manager.run()
+    db.create_all()
+    db.session.commit()
