7e93d0d88f96c032125e5817c0e607f6a2e1d20a
krood20 <krood20@gwmail.gwu.edu>
2018-06-15 12:53:56 -0400

    trying to successfully pass in long and lat

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -50,10 +50,10 @@ def homepage():
     image_count = conn.fetchone()[0]
     image_count = '{:,}'.format(image_count)
 
-    lng = np.array(all_cameras)[:,4]
-    la = np.array(all_cameras)[:,3]
+    long = np.array(all_cameras)[:,4]
+    lat = np.array(all_cameras)[:,3]
 
-    return render_template('home.html', long=lng, lat=la, camera_count=camera_count, image_count=image_count)
+    return render_template('home.html', lg=lng, lt=lat, camera_count=camera_count, image_count=image_count)
 
 
 @app.route('/cameras/<int:ind>/')
