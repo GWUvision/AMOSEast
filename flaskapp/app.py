be644b219d44b0d4f34173dd9dd7dcc6f4853bc5
sshah98 <shahsuraj261@gmail.com>
2018-06-18 10:01:35 -0400

    added changes to show date properly

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -4,10 +4,10 @@ import os
 import numpy as np
 
 from flask import Flask, render_template, request, redirect, url_for, g
-from pager import Pager
 from flask_sqlalchemy import SQLAlchemy
+from flask_moment import Moment
+from pager import Pager
 from weather import Weather, Unit
-
 
 STATIC_FOLDER = 'static'
 APPNAME = 'AMOS East'
@@ -15,13 +15,14 @@ APPNAME = 'AMOS East'
 app = Flask(__name__, static_folder=STATIC_FOLDER)
 app.config.update(APPNAME=APPNAME,)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+moment = Moment(app)
 db = SQLAlchemy(app)
 
 from models import *
 
 DATABASE_URL = os.environ['DATABASE_URL']
-
 conn = psycopg2.connect(DATABASE_URL, sslmode='require').cursor()
 all_cameras_query = "SELECT cameraid, name, url, latitude, longitude FROM cameras ORDER BY cameraid"
 conn.execute(all_cameras_query)
@@ -50,8 +51,8 @@ def homepage():
     image_count = conn.fetchone()[0]
     image_count = '{:,}'.format(image_count)
 
-    lng = np.array(all_cameras)[:,4]
-    lat = np.array(all_cameras)[:,3]
+    lng = np.array(all_cameras)[:, 4]
+    lat = np.array(all_cameras)[:, 3]
 
     return render_template('home.html', lg=lng, lt=lat, camera_count=camera_count, image_count=image_count)
 
@@ -75,9 +76,11 @@ def directory_view(ind=1):
 
                 weather = Weather(unit=Unit.FAHRENHEIT)
 
-                lookup = weather.lookup_by_latlng(all_cameras[ind][3], all_cameras[ind][4])
+                lookup = weather.lookup_by_latlng(
+                    all_cameras[ind][3], all_cameras[ind][4])
                 condition = lookup.condition
-                w_info = [condition.temp +'\N{DEGREE SIGN}F and ' + condition.text]
+                w_info = [condition.temp +
+                          '\N{DEGREE SIGN}F and ' + condition.text]
             except AttributeError as e:
                 w_info = ['No weather information available']
                 return render_template('dirview.html', index=ind, pager=pager, data=all_cameras[ind], data2=camera_images[-1], weather=w_info)
@@ -164,12 +167,14 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
+
 @app.route('/cluster_test')
 def cluster_test():
-    lng = np.array(all_cameras)[:,4]
-    lat = np.array(all_cameras)[:,3]
+    lng = np.array(all_cameras)[:, 4]
+    lat = np.array(all_cameras)[:, 3]
 
     return render_template('cluster_test.html', lg=lng, lt=lat)
 
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
