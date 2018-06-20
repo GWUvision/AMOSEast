5f1e69e0ab2bc3f3d88fba6c37b5340c3ef96d48
krood20 <krood20@gwmail.gwu.edu>
2018-06-20 12:46:23 -0400

    finished log file for now, adding to server

---
+++ flaskapp/log.py
@@ -0,0 +1,48 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+import os
+from flask import Flask, render_template, request, redirect, url_for, g
+import psycopg2
+
+from app import db
+
+# Get the camera and image counts from the database
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='allow').cursor()
+all_cameras_query = "SELECT COUNT(*) FROM cameras"
+conn.execute(all_cameras_query)
+camera_count = conn.fetchall()
+
+all_cameras_query = "SELECT COUNT(*) FROM images"
+conn.execute(all_cameras_query)
+total_image_count = conn.fetchall()
+
+all_cameras_query = "SELECT COUNT(*) FROM images WHERE Date(curr_time) = '" + \
+    str(datetime.today().strftime('%Y-%m-%d')) + "'"
+conn.execute(all_cameras_query)
+image_count = conn.fetchall()
+
+
+# Stuff for actually emailing
+fromaddr = "kylerood16@gmail.com"
+toaddr = "kyzle@me.com"
+
+msg = MIMEMultipart()
+msg['From'] = fromaddr
+msg['To'] = toaddr
+subj = "LOG FOR " + str(datetime.now())
+msg['Subject'] = subj
+
+body = "LOG FILE FOR " + str(datetime.now()) + "\nNumber of Cams: " + str(camera_count[0][0]) + "\nNumber of Images total: " + str(total_image_count[0][0]) + "\nNumber of Images Captured Today: " + str(image_count[0][0])
+msg.attach(MIMEText(body, 'plain'))
+
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(fromaddr, "Andreschurrle9")
+text = msg.as_string()
+server.sendmail(fromaddr, toaddr, text)
+server.quit()
+exit()
