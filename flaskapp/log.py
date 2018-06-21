3ed62a79670c2fa9898f9f2853cb860d93e74be0
sshah98 <shahsuraj261@gmail.com>
2018-06-21 14:53:22 -0400

    modified log to display time nicely

--- flaskapp/log.py
+++ flaskapp/log.py
@@ -1,17 +1,21 @@
-import smtplib
+import os
 import psutil
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
+import psycopg2
+import smtplib
+import time
+
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
-import os
 from flask import Flask, render_template, request, redirect, url_for, g
-import psycopg2
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 from app import db
 
+
 def getCPUuse():
     return(str(os.popen("top -n1 | awk '/Cpu\(s\):/ {print $2}'").readline().strip()))
+
 
 # Get the camera and image counts from the database
 DATABASE_URL = os.environ['DATABASE_URL']
@@ -30,9 +34,9 @@ conn.execute(all_cameras_query)
 image_count = conn.fetchall()
 
 
-#getting disk and cpu inforamtion
+# getting disk and cpu inforamtion
 stats = os.statvfs('../../../../../pless_nfs')
-free = str(float(stats.f_bfree)/float(stats.f_blocks))
+free = str(float(stats.f_bfree) / float(stats.f_blocks))
 cpu = str(psutil.cpu_percent(interval=1))
 
 
@@ -42,14 +46,17 @@ toaddr = "krood20@gwmail.gwu.edu"
 cc0 = "robert.pless@gmail.com"
 cc1 = "shahsuraj261@gmail.com"
 
+time.ctime()
+curr_time = time.strftime('%b %d, %Y %l:%M%p %Z')
+
 msg = MIMEMultipart()
 msg['From'] = fromaddr
 msg['To'] = toaddr
-msg['Cc'] = cc0
-subj = "LOG FOR " + str(datetime.now())
+subj = "LOG FOR " + curr_time
 msg['Subject'] = subj
 
-body = "LOG FILE FOR " + str(datetime.now()) + "\nNumber of Cams: " + str(camera_count[0][0]) + "\nNumber of Images total: " + str(total_image_count[0][0]) + "\nNumber of Images Captured Today: " + str(image_count[0][0]) + "\nPercent of disk free: " + free + "\nPercent of CPU currently in use: " + cpu
+body = "LOG FILE FOR " + curr_time + "\nNumber of Cams: " + str(camera_count[0][0]) + "\nNumber of Images total: " + str(
+    total_image_count[0][0]) + "\nNumber of Images Captured Today: " + str(image_count[0][0]) + "\nPercent of disk free: " + free + "\nPercent of CPU currently in use: " + cpu
 msg.attach(MIMEText(body, 'plain'))
 
 server = smtplib.SMTP('smtp.gmail.com', 587)
