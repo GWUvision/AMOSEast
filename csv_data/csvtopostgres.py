8a6c42bb8ae9e62a2ec6380ec386e22096141e22
sshah98 <shahsuraj261@gmail.com>
2018-06-25 13:08:03 -0400

    made change to csv to postgres

--- csv_data/csvtopostgres.py
+++ csv_data/csvtopostgres.py
@@ -5,14 +5,14 @@ DATABASE_URL = os.environ['DATABASE_URL']
 database = psycopg2.connect(DATABASE_URL, sslmode='allow')
 
 cur = database.cursor()
-root = 'csv_data/data2.csv'
+root = '/home/suraj/Documents/GWU/AMOSEast/csv_data/data2.csv'
 
 print("Connected to database...")
 
 # psql -c "\copy cameras FROM 'root' delimiter ',' csv header"
 
 try:
-    sql = "COPY cameras(cameraid, name, url, latitude, longitude, last_width, last_height) FROM '%s' DELIMITER ',' CSV HEADER" % (root)
+    sql = "COPY cameras(cameraid, name, url, latitude, longitude, last_width, last_height, mhash) FROM '%s' DELIMITER ',' CSV HEADER" % (root)
     cur.execute(sql)
     print("Copied data from csv to postgres database...")
 
