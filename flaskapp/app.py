1ca73fe495396315e2369cd376d846cdcfae7c5c
sshah98 <shahsuraj261@gmail.com>
2018-06-25 14:38:02 -0400

    trying to debug cool cams

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -168,7 +168,7 @@ def allcamspage():
         current_image_query = "SELECT filepath from images where cameraid=%d ORDER BY curr_time DESC" % (
             cameraid)
         conn.execute(current_image_query)
-        current_image = conn.fetchone()[0]
+        current_image = conn.fetchone()
 
         # index subtract by 1 because all_cams starts at 0 while the camera list starts at 1; getting the name for each camera
         camera_name = all_cameras[cameraid - 1][1]
