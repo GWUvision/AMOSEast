4b6ed7d1a6abca05867daf462befcbb0c992ab04
krood20 <krood20@gwmail.gwu.edu>
2018-06-26 15:21:13 -0400

    changed up the distance file

--- distance.py
+++ distance.py
@@ -2,7 +2,7 @@ import math
 import psycopg2
 import os
 import pysal
-from pysal.cg.kdtree import KDTree    
+from pysal.cg.kdtree import KDTree
 
 DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='allow').cursor()
@@ -16,18 +16,21 @@ locations = []
 for cameras in all_cameras:
     locations.append((cameras[3], cameras[4]))
 
+good_cams = []
+
 for i in range(0, len(all_cameras)):
     current_point = (all_cameras[i][3], all_cameras[i][4])
-    
+
+    for j in range(0, len(all_cameras)):
+        tree = KDTree(locations, distance_metric='Arc', radius=pysal.cg.RADIUS_EARTH_MILES)
+
+        # # get all points within 1 mile of 'current_point'
+        indices = tree.query_ball_point(current_point, 20)
+    #if there are no indices, then add it to the list, because this means there are no cams in 20 mile radius
+    if(!indices):
+        good_cams.append(i)
+        print(str(i) + " is a good cam!")
     # print(current_point)
-    
-    tree = KDTree(locations, distance_metric='Arc', radius=pysal.cg.RADIUS_EARTH_MILES)
-# 
-# 
-# # current_point = (44.478739, -73.19164)
-# 
-# # get all points within 1 mile of 'current_point'
-    indices = tree.query_ball_point(current_point, 20)
-    
-    for i in indices:
-        print(all_cameras[i])
+
+    #for i in indices:
+        #print(all_cameras[i])
