d9845d37da87c2424f48c41d97c7fde07d9a3a23
krood20 <krood20@gwmail.gwu.edu>
2018-06-27 13:22:06 -0400

    added threading

--- distance.py
+++ distance.py
@@ -5,7 +5,9 @@ import pysal
 import sys
 import csv
 from pysal.cg.kdtree import KDTree
+from gevent.pool import Pool
 
+pool = Pool(30)
 sys.setrecursionlimit(10000)
 csvfile = "good_cams.csv"
 
@@ -18,33 +20,37 @@ conn.execute(all_cameras_query)
 all_cameras = conn.fetchall()
 
 locations = []
-for cameras in all_cameras:
-    locations.append((cameras[3], cameras[4]))
 
-#list of good camera indexes
-good_cams = []
+def get_cams():
+    for cameras in all_cameras:
+        locations.append((cameras[3], cameras[4]))
 
-for i in range(0, len(all_cameras)):
-    current_point = (all_cameras[i][3], all_cameras[i][4])
-    print("Checking cam " + str(i))
-    indices = None
+    #list of good camera indexes
+    good_cams = []
 
-    for j in range(0, len(all_cameras)):
-        if(i == j):
-            break
-        else:
-            tree = KDTree(locations, distance_metric='Arc', radius=pysal.cg.RADIUS_EARTH_MILES)
-            # # get all points within 1 mile of 'current_point'
-            indices = tree.query_ball_point(current_point, 20)
+    for i in range(0, len(all_cameras)):
+        current_point = (all_cameras[i][3], all_cameras[i][4])
+        print("Checking cam " + str(i))
+        indices = None
 
-    #if there are no indices, then add it to the list, because this means there are no cams in 20 mile radius
-    if not indices:
-        good_cams.append(i)
-        print(str(i) + " is a good cam!")
+        for j in range(0, len(all_cameras)):
+            if(i == j):
+                break
+            else:
+                tree = KDTree(locations, distance_metric='Arc', radius=pysal.cg.RADIUS_EARTH_MILES)
+                # # get all points within 1 mile of 'current_point'
+                indices = tree.query_ball_point(current_point, 20)
+
+        #if there are no indices, then add it to the list, because this means there are no cams in 20 mile radius
+        if not indices:
+            good_cams.append(i)
+            print(str(i) + " is a good cam!")
 
 
-#write to the csv
-with open(csvfile, "w") as output:
-    writer = csv.writer(output, lineterminator='\n')
-    for val in good_cams:
-        writer.writerow([val])
+    #write to the csv
+    with open(csvfile, "w") as output:
+        writer = csv.writer(output, lineterminator='\n')
+        for val in good_cams:
+            writer.writerow([val])
+
+jobs = [pool.spawn(get_cams)]
