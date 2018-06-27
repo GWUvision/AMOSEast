451baefa5fd8fb004f051f236f4fc75f9fb85a26
krood20 <krood20@gwmail.gwu.edu>
2018-06-27 14:31:58 -0400

    debugging threading

--- distance.py
+++ distance.py
@@ -27,8 +27,8 @@ for camera in all_cameras:
 def get_cams(latitude, longitude):
     #list of good camera indexes
     good_cams = []
-    locations = []
-    locations.append((latitude, longitude))
+    location = []
+    location.append((latitude, longitude))
 
     for i in range(0, len(all_cameras)):
         current_point = (all_cameras[i][3], all_cameras[i][4])
@@ -39,7 +39,7 @@ def get_cams(latitude, longitude):
             if(i == j):
                 break
             else:
-                tree = KDTree(locations, distance_metric='Arc', radius=pysal.cg.RADIUS_EARTH_MILES)
+                tree = KDTree(location, distance_metric='Arc', radius=pysal.cg.RADIUS_EARTH_MILES)
                 # # get all points within 1 mile of 'current_point'
                 indices = tree.query_ball_point(current_point, 20)
 
