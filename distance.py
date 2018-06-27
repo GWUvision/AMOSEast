91e8e7e58b574b60cec9839d54ce125aece1b9e7
krood20 <krood20@gwmail.gwu.edu>
2018-06-27 12:53:14 -0400

    syntax error

--- distance.py
+++ distance.py
@@ -24,6 +24,7 @@ good_cams = []
 for i in range(0, len(all_cameras)):
     current_point = (all_cameras[i][3], all_cameras[i][4])
     print("Checking cam " + str(i))
+    indices = None
 
     for j in range(0, len(all_cameras)):
         if(i == j):
@@ -33,7 +34,7 @@ for i in range(0, len(all_cameras)):
             # # get all points within 1 mile of 'current_point'
             indices = tree.query_ball_point(current_point, 20)
     #if there are no indices, then add it to the list, because this means there are no cams in 20 mile radius
-    
+
     if not indices:
         good_cams.append(i)
         print(str(i) + " is a good cam!")
