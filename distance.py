a98f888279801d832f65b332821b9bf504bdc369
krood20 <krood20@gwmail.gwu.edu>
2018-06-27 12:50:31 -0400

    fixing logic a bit

--- distance.py
+++ distance.py
@@ -23,13 +23,17 @@ good_cams = []
 
 for i in range(0, len(all_cameras)):
     current_point = (all_cameras[i][3], all_cameras[i][4])
+    print("Checking cam " + str(i))
 
     for j in range(0, len(all_cameras)):
-        tree = KDTree(locations, distance_metric='Arc', radius=pysal.cg.RADIUS_EARTH_MILES)
-
-        # # get all points within 1 mile of 'current_point'
-        indices = tree.query_ball_point(current_point, 20)
+        if(i == j):
+            break
+        else:
+            tree = KDTree(locations, distance_metric='Arc', radius=pysal.cg.RADIUS_EARTH_MILES)
+            # # get all points within 1 mile of 'current_point'
+            indices = tree.query_ball_point(current_point, 20)
     #if there are no indices, then add it to the list, because this means there are no cams in 20 mile radius
+    
     if not indices:
         good_cams.append(i)
         print(str(i) + " is a good cam!")
