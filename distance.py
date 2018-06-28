30303e5a6c7798baf74002af2f333621f3faad0b
krood20 <krood20@gwmail.gwu.edu>
2018-06-28 10:24:01 -0400

    changed up the distance file again

--- distance.py
+++ distance.py
@@ -8,10 +8,10 @@ from pysal.cg.kdtree import KDTree
 from gevent import monkey, socket
 from gevent.pool import Pool
 
-monkey.patch_socket()
-pool = Pool(30)
-sys.setrecursionlimit(10000)
-csvfile = "good_cams.csv"
+# monkey.patch_socket()
+# pool = Pool(30)
+# sys.setrecursionlimit(10000)
+# csvfile = "good_cams.csv"
 
 DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='allow').cursor()
@@ -25,33 +25,77 @@ for camera in all_cameras:
 
 good_cams = []
 
-def get_cams(latitude, longitude):
-    for i in range(0, len(all_cameras)):
-        #current_point = (all_cameras[i][3], all_cameras[i][4])
-        current_point = (latitude, longitude)
-        print("Checking cam " + str(i))
-        indices = None
 
-        for j in range(0, len(all_cameras)):
-            if(i == j):
-                break
-            else:
-                tree = KDTree(locations, distance_metric='Arc', radius=pysal.cg.RADIUS_EARTH_MILES)
-                # # get all points within 1 mile of 'current_point'
-                indices = tree.query_ball_point(current_point, 20)
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
 
-        #if there are no indices, then add it to the list, because this means there are no cams in 20 mile radius
-        if not indices:
-            good_cams.append(i)
-            print(str(i) + " is a good cam!")
+    # haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a))
+    r = 6371 # Radius of earth in kilometers. Use 3956 for miles
+    return c * r
+
+#some globals we need
+radius = 30.0 # in kilometer
+good = True #0 means it is a good camera
+
+for i in range(0, len(all_cameras)):
+    lat1 = locations[0][0]
+    lon1 = locations[0][1]
+
+    lat2 = locations[i][0]
+    lon2 = locations[i][1]
+
+    a = haversine(lon1, lat1, lon2, lat2)
 
 
-    #write to the csv
-    with open(csvfile, "a") as output:
-        writer = csv.writer(output, lineterminator='\n')
-        for val in good_cams:
-            writer.writerow([val])
+    print('Distance (km) : ', a)
+    if a <= radius:
+        good = False #1 means it is no longer a default good cam
 
-jobs = [pool.spawn(get_cams, latitude, longitude)
-            for latitude, longitude in locations]
-print("Acquired cams")
+if(good):
+    print("Good cam!")
+
+
+a = haversine(lon1, lat1, lon2, lat2)
+
+
+
+
+# def get_cams(latitude, longitude):
+#     for i in range(0, len(all_cameras)):
+#         #current_point = (all_cameras[i][3], all_cameras[i][4])
+#         current_point = (latitude, longitude)
+#         print("Checking cam " + str(i))
+#         indices = None
+#
+#         for j in range(0, len(all_cameras)):
+#             if(i == j):
+#                 break
+#             else:
+#                 tree = KDTree(locations, distance_metric='Arc', radius=pysal.cg.RADIUS_EARTH_MILES)
+#                 # # get all points within 20 mile of 'current_point'
+#                 indices = tree.query_ball_point(current_point, 20)
+#
+#         #if there are no indices, then add it to the list, because this means there are no cams in 20 mile radius
+#         if not indices:
+#             good_cams.append(i)
+#             print(str(i) + " is a good cam!")
+#
+#
+#     #write to the csv
+#     with open(csvfile, "a") as output:
+#         writer = csv.writer(output, lineterminator='\n')
+#         for val in good_cams:
+#             writer.writerow([val])
+#
+# jobs = [pool.spawn(get_cams, latitude, longitude)
+#             for latitude, longitude in locations]
+# print("Acquired cams")
