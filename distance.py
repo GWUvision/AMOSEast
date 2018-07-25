03e87413a688ad69bc34ae8f7a82d2fa10a19967
krood20 <krood20@gwmail.gwu.edu>
2018-07-25 10:54:41 -0400

    made a script to combine the csv's, we now have a list of all the cameras we need

--- distance.py
+++ distance.py
@@ -10,29 +10,23 @@ from gevent.pool import Pool
 import pandas as pd
 from gmplot import gmplot
 
-# monkey.patch_socket()
-# pool = Pool(30)
-# sys.setrecursionlimit(10000)
+
 csvfile = "distance.csv"
 
 # Place map
 gmap = gmplot.GoogleMapPlotter(0, 0, 13)
 
-#Get info from the database
-# DATABASE_URL = os.environ['DATABASE_URL']
-# conn = psycopg2.connect(DATABASE_URL, sslmode='allow').cursor()
-# all_cameras_query = "SELECT cameraid, name, url, latitude, longitude FROM cameras ORDER BY cameraid"
-# conn.execute(all_cameras_query)
-# all_cameras = conn.fetchall()
-
 #reding from csv instead of database
 df = pd.read_csv('ratings.csv')
 print(df)
 
+#getting the locations and camera ids
 locations = []
+ids = []
 
 for index, row in df.iterrows():
     locations.append((row['latitude'], row['longitude']))
+    ids.append(row['cameraid'])
 
 def haversine(lon1, lat1, lon2, lat2):
     """
@@ -53,10 +47,10 @@ def haversine(lon1, lat1, lon2, lat2):
 #some globals we need
 radius = 30.0 # in kilometer
 good_cams = [(locations[0][0], locations[0][1])]
-index = []
+good_index = [ids[0]]
 
 for i in range(1, len(locations)):
-    print("Checking cam " + str(i))
+    print("Checking cam " + str(ids[i]))
 
     lat1 = locations[i][0]
     lon1 = locations[i][1]
@@ -77,9 +71,9 @@ for i in range(1, len(locations)):
 
     lat, lon = locations[i][0], locations[i][1]
     if(good):
-        print(str(i) + " is a good cam!")
+        print(str(ids[i]) + " is a good cam!")
         good_cams.append((locations[i][0], locations[i][1]))
-        index.append(str(i))
+        good_index.append(ids[i])
 
         #add marker to map
         gmap.marker(lat, lon, 'cornflowerblue')
@@ -88,42 +82,9 @@ for i in range(1, len(locations)):
         gmap.marker(lat, lon, 'red')
 
 with open(csvfile, "a") as output:
-    writer = csv.writer(output, lineterminator='\n')
-    for val in index:
+    writer = csv.writer(output, delimiter=',', lineterminator='\n')
+    for val in good_index:
         writer.writerow([val])
 
 # Draw
 gmap.draw("my_map.html")
-
-
-
-# def get_cams(latitude, longitude):
-#     for i in range(0, len(all_cameras)):
-#         #current_point = (all_cameras[i][3], all_cameras[i][4])
-#         current_point = (latitude, longitude)
-#         print("Checking cam " + str(i))
-#         indices = None
-#
-#         for j in range(0, len(all_cameras)):
-#             if(i == j):
-#                 break
-#             else:
-#                 tree = KDTree(locations, distance_metric='Arc', radius=pysal.cg.RADIUS_EARTH_MILES)
-#                 # # get all points within 20 mile of 'current_point'
-#                 indices = tree.query_ball_point(current_point, 20)
-#
-#         #if there are no indices, then add it to the list, because this means there are no cams in 20 mile radius
-#         if not indices:
-#             good_cams.append(i)
-#             print(str(i) + " is a good cam!")
-#
-#
-#     #write to the csv
-#     with open(csvfile, "a") as output:
-#         writer = csv.writer(output, lineterminator='\n')
-#         for val in good_cams:
-#             writer.writerow([val])
-#
-# jobs = [pool.spawn(get_cams, latitude, longitude)
-#             for latitude, longitude in locations]
-# print("Acquired cams")
