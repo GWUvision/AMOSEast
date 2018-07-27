a670858d3491ee19af38451803e432a687276ea0
krood20 <krood20@gwmail.gwu.edu>
2018-07-27 13:37:53 -0400

    debugging pager

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -85,7 +85,9 @@ def directory_view(ind=1):
     results = Camera.query.filter_by(cameraid=ind + 1).first()
     pager = Pager(db.engine.execute(
         'select count(cameraid) from cameras').scalar())
-        
+
+    print(results)
+
     try:
 
         if ind >= pager.count:
