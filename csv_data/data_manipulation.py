a29a29c1da06ed1a7f2cc47479a438de50239022
sshah98 <shahsuraj261@gmail.com>
2018-08-06 13:53:28 -0400

    changed some stuff

--- csv_data/data_manipulation.py
+++ csv_data/data_manipulation.py
@@ -7,37 +7,50 @@ import numpy as np
 from pprint import pprint
 
 
-for i in range(1, 71, 1):
-
-    url = 'http://amos.cse.wustl.edu/REST/webcams/?page={0}&format=json'.format(i)
-    with urllib.request.urlopen(url) as url:
-        data = json.loads(url.read().decode())
-
-    df = pd.DataFrame.from_dict(data['results'])
-    print(df.head())
-    df.to_csv('output.csv', mode='a', header=False)
-    print('Page {0} done...'.format(i))
-
-df1 = pd.read_csv('output.csv')    
-
-
-
-
-
-# df = pd.read_csv('plessurl_list.csv')
-# df = df[np.isfinite(df['roberts auto-rating'])]
+# for i in range(1, 71, 1):
 # 
-# df.drop(['roberts auto-rating', 'index2'], axis=1, inplace=True)
+#     url = 'http://amos.cse.wustl.edu/REST/webcams/?page={0}&format=json'.format(i)
+#     with urllib.request.urlopen(url) as url:
+#         data = json.loads(url.read().decode())
 # 
-# df['want_kept'] = np.where((df['rating'] != df['extraKeeps']), df['extraKeeps'], np.nan)
-# df = df[np.isfinite(df['want_kept'])]
-# 
-# df.drop(['extraKeeps', 'rating'], axis=1, inplace=True)
-# 
-# 
-# df.to_csv('test.csv')
-# print(df.to_string())
-# print(df.to_string())
+#     df = pd.DataFrame.from_dict(data['results'])
+#     print(df.head())
+#     df.to_csv('output.csv', mode='a', header=False)
+#     print('Page {0} done...'.format(i))
+
+# df1 = pd.read_csv('interesting_cams.csv')
+# df2 = pd.read_csv('old_data.csv')    
+# df = pd.merge(df1, df2, on=['url'], how='left', indicator='old_cameraid')
+# # print(df.head(200).to_string())
+# df.to_csv('output.csv')
+
+
+# =========================================================
+
+# df1 = pd.read_csv('interesting_images.csv')
+
+# df2 = pd.read_csv('cameraid_data.csv')
+# df = pd.merge(df1, df2, on=['cameraid'], how='left', indicator='test')
+
+# df.to_csv('new_image_data.csv')
+
+# print(df.head())
+
+# =============================================================
+
+df = pd.read_csv('new_image_data.csv')
+
+# df.apply(lambda x: x['filepath'].replace(,x['b']), axis=1)
+
+df['filepath'] = df['old_cameraid'].astype(str) + '/' + df['filepath'].astype(str).map(lambda x: x.split('/')[-1])
+
+
+df.to_csv('new_interesting_images.csv')
+
+# print(df.head())
+
+
+
 
 
 
