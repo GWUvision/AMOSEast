eae279dd73824af95e9138555f65470ce87ea472
sshah98 <shahsuraj261@gmail.com>
2018-08-08 11:09:39 -0400

    working on traversing files

--- csv_data/data_manipulation.py
+++ csv_data/data_manipulation.py
@@ -8,18 +8,18 @@ from pprint import pprint
 
 
 # for i in range(1, 71, 1):
-# 
+#
 #     url = 'http://amos.cse.wustl.edu/REST/webcams/?page={0}&format=json'.format(i)
 #     with urllib.request.urlopen(url) as url:
 #         data = json.loads(url.read().decode())
-# 
+#
 #     df = pd.DataFrame.from_dict(data['results'])
 #     print(df.head())
 #     df.to_csv('output.csv', mode='a', header=False)
 #     print('Page {0} done...'.format(i))
 
 # df1 = pd.read_csv('interesting_cams.csv')
-# df2 = pd.read_csv('old_data.csv')    
+# df2 = pd.read_csv('old_data.csv')
 # df = pd.merge(df1, df2, on=['url'], how='left', indicator='old_cameraid')
 # # print(df.head(200).to_string())
 # df.to_csv('output.csv')
@@ -38,19 +38,24 @@ from pprint import pprint
 
 # =============================================================
 
-df = pd.read_csv('new_image_data.csv')
-# df.apply(lambda x: x['filepath'].replace(,x['b']), axis=1)
-
-df['filepath'] = df['old_cameraid'].astype(str) + '/' + df['filepath'].astype(str).map(lambda x: x.split('/')[-1])
-
-df.to_csv('new_interesting_images.csv')
+# df = pd.read_csv('new_image_data.csv')
+# # df.apply(lambda x: x['filepath'].replace(,x['b']), axis=1)
+#
+# df['filepath'] = df['old_cameraid'].astype(str) + '/' + df['filepath'].astype(str).map(lambda x: x.split('/')[-1])
+#
+# df.to_csv('new_interesting_images.csv')
 
 # print(df.head())
 
 
 # TODO: add leading 0's, 999000, all cameras should have six digit
 
+import os
 
-
+for root, dirs, files in os.walk('flaskapp/static/images'):
+    path = root.split(os.sep)
+    print((len(path) - 1) * '---', os.path.basename(root))
+    for file in files:
+        print(len(path) * '---', file)
 
 
