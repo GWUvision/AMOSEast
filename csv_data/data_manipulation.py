0ba380ddb0d81d31f37b227f9e008d98d9ec7cfb
krood20 <krood20@gwmail.gwu.edu>
2018-08-10 11:23:48 -0400

    changed filepaths slightly

--- csv_data/data_manipulation.py
+++ csv_data/data_manipulation.py
@@ -55,7 +55,7 @@ import os
 data = pd.read_csv("../new_interesting_cams.csv")
 
 #first change the name of the files
-for root, dirs, files in os.walk('../flaskapp/static/images'):
+for root, dirs, files in os.walk('../flaskapp/static/images3/images'):
     path = root.split(os.sep)
     print('Subdir: ', (len(path) - 1) * '---', os.path.basename(root))
 
@@ -70,7 +70,7 @@ for root, dirs, files in os.walk('../flaskapp/static/images'):
                 os.rename(os.path.join(root, f), os.path.join(root, new_file))
 
 #then give the folders a temporary name to avoid collisions
-for root, dirs, files in os.walk('../flaskapp/static/images'):
+for root, dirs, files in os.walk('../flaskapp/static/images3/images'):
     path = root.split(os.sep)
     cwd = os.getcwd()
     size = len(path) - 1
@@ -79,10 +79,10 @@ for root, dirs, files in os.walk('../flaskapp/static/images'):
         continue
     else:
         #print(path[size])
-        os.rename(cwd + '/../flaskapp/static/images/' + path[size], cwd + '/../flaskapp/static/images/a' + path[size])
+        os.rename(cwd + '/../flaskapp/static/images3/images/' + path[size], cwd + '/../flaskapp/static/images3/images/a' + path[size])
 
 #lastly put the actual directory name in
-for root, dirs, files in os.walk('../flaskapp/static/images'):
+for root, dirs, files in os.walk('../flaskapp/static/images3/images'):
     path = root.split(os.sep)
     cwd = os.getcwd()
     size = len(path) - 1
@@ -100,4 +100,4 @@ for root, dirs, files in os.walk('../flaskapp/static/images'):
             if(d == str(data['cameraid'][i])):
                 final_dir = str(data['old_cameraid'][i])
                 final = final_dir.zfill(6)
-                os.rename(cwd + '/../flaskapp/static/images/' + path[size], cwd + '/../flaskapp/static/images/' + final)
+                os.rename(cwd + '/../flaskapp/static/images3/images/' + path[size], cwd + '/../flaskapp/static/images3/images/' + final)
