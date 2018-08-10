55d90724911136e2362a61c85cf78c26860d0ac5
krood20 <krood20@gwmail.gwu.edu>
2018-08-10 12:47:18 -0400

    added actual csv

--- csv_data/data_manipulation.py
+++ csv_data/data_manipulation.py
@@ -55,49 +55,55 @@ import os
 data = pd.read_csv('../new_interesting_cams.csv')
 
 #first change the name of the files
-for root, dirs, files in os.walk('../flaskapp/static/images3/images'):
+for root, dirs, files in os.walk('../test_images'):
     path = root.split(os.sep)
     print('Subdir: ', (len(path) - 1) * '---', os.path.basename(root))
 
     for i in range(0, len(data['cameraid'])):
-        #print(str(data['old_cameraid'][i]))
-        if(os.path.basename(root) == str(data['cameraid'][i])):
+
+        print(str(data['cameraid'][i]))
+        print(os.path.basename(root))
+
+        if(os.path.basename(root) == str(int(data['cameraid'][i]))):
+            print('here')
             for f in files:
                 old_file = list(f)
                 index = f.index('_')
                 new_num = str(data['old_cameraid'][i])
-                new_file = new_num.zfill(6) + f[index:] #used to have zfill here, suraj changed it in database
+                print(new_num)
+                new_file = new_num.zfill(6) + f[index:]
+                print(new_file)
                 os.rename(os.path.join(root, f), os.path.join(root, new_file))
 
 #then give the folders a temporary name to avoid collisions
-for root, dirs, files in os.walk('../flaskapp/static/images3/images'):
-    path = root.split(os.sep)
-    cwd = os.getcwd()
-    size = len(path) - 1
-
-    if(path[size] == 'images'):
-        continue
-    else:
-        #print(path[size])
-        os.rename(cwd + '/../flaskapp/static/images3/images/' + path[size], cwd + '/../flaskapp/static/images3/images/a' + path[size])
-
-#lastly put the actual directory name in
-for root, dirs, files in os.walk('../flaskapp/static/images3/images'):
-    path = root.split(os.sep)
-    cwd = os.getcwd()
-    size = len(path) - 1
-
-    if(path[size] == 'images'):
-        continue
-    else:
-        print(path[size])
-        d = str(path[size])
-        d = d[1:]
-        print(d)
-        final = ''
-
-        for i in range(0, len(data['cameraid'])):
-            if(d == str(data['cameraid'][i])):
-                final_dir = str(data['old_cameraid'][i])
-                final = final_dir.zfill(6)
-                os.rename(cwd + '/../flaskapp/static/images3/images/' + path[size], cwd + '/../flaskapp/static/images3/images/' + final)
+# for root, dirs, files in os.walk('../test_images'):
+#     path = root.split(os.sep)
+#     cwd = os.getcwd()
+#     size = len(path) - 1
+#
+#     if(path[size] == 'test_images'):
+#         continue
+#     else:
+#         #print(path[size])
+#         os.rename(cwd + '/../test_images/' + path[size], cwd + '/../test_images/a' + path[size])
+#
+# #lastly put the actual directory name in
+# for root, dirs, files in os.walk('../test_images'):
+#     path = root.split(os.sep)
+#     cwd = os.getcwd()
+#     size = len(path) - 1
+#
+#     if(path[size] == 'test_images'):
+#         continue
+#     else:
+#         print(path[size])
+#         d = str(path[size])
+#         d = d[1:]
+#         print(d)
+#         final = ''
+#
+#         for i in range(0, len(data['cameraid'])):
+#             if(d == str(data['cameraid'][i])):
+#                 final_dir = str(data['old_cameraid'][i])
+#                 final = final_dir.zfill(6)
+#                 os.rename(cwd + '/../test_images/' + path[size], cwd + '/../test_images/' + final)
